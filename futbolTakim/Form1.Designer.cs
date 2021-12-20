e5eae24a44897e67aa442a7639f557244629b56c
MertCanCelebi <mertcancelebi12@gmail.com>
2021-12-20 15:24:07 +0300

    Initial commit

---
+++ futbolTakim/Form1.Designer.cs
@@ -0,0 +1,307 @@
+﻿
+namespace futbolTakim
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtTakimNo = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtTcno = new System.Windows.Forms.TextBox();
+            this.txtAdi = new System.Windows.Forms.TextBox();
+            this.txtDeger = new System.Windows.Forms.TextBox();
+            this.txtYas = new System.Windows.Forms.TextBox();
+            this.txtUlkeKodu = new System.Windows.Forms.TextBox();
+            this.txtSozlesmeId = new System.Windows.Forms.TextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnAra = new System.Windows.Forms.Button();
+            this.btnGuncelle = new System.Windows.Forms.Button();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.btnListele = new System.Windows.Forms.Button();
+            this.dgwOyuncu = new System.Windows.Forms.DataGridView();
+            this.txtAra = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgwOyuncu)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtTakimNo
+            // 
+            this.txtTakimNo.Location = new System.Drawing.Point(589, 251);
+            this.txtTakimNo.Name = "txtTakimNo";
+            this.txtTakimNo.Size = new System.Drawing.Size(89, 20);
+            this.txtTakimNo.TabIndex = 117;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(497, 254);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 116;
+            this.label1.Text = "TAKIM NO";
+            // 
+            // txtTcno
+            // 
+            this.txtTcno.Location = new System.Drawing.Point(589, 95);
+            this.txtTcno.Name = "txtTcno";
+            this.txtTcno.Size = new System.Drawing.Size(89, 20);
+            this.txtTcno.TabIndex = 115;
+            // 
+            // txtAdi
+            // 
+            this.txtAdi.Location = new System.Drawing.Point(589, 124);
+            this.txtAdi.Name = "txtAdi";
+            this.txtAdi.Size = new System.Drawing.Size(89, 20);
+            this.txtAdi.TabIndex = 114;
+            // 
+            // txtDeger
+            // 
+            this.txtDeger.Location = new System.Drawing.Point(589, 150);
+            this.txtDeger.Name = "txtDeger";
+            this.txtDeger.Size = new System.Drawing.Size(89, 20);
+            this.txtDeger.TabIndex = 113;
+            // 
+            // txtYas
+            // 
+            this.txtYas.Location = new System.Drawing.Point(589, 176);
+            this.txtYas.Name = "txtYas";
+            this.txtYas.Size = new System.Drawing.Size(89, 20);
+            this.txtYas.TabIndex = 112;
+            // 
+            // txtUlkeKodu
+            // 
+            this.txtUlkeKodu.Location = new System.Drawing.Point(589, 202);
+            this.txtUlkeKodu.Name = "txtUlkeKodu";
+            this.txtUlkeKodu.Size = new System.Drawing.Size(89, 20);
+            this.txtUlkeKodu.TabIndex = 111;
+            // 
+            // txtSozlesmeId
+            // 
+            this.txtSozlesmeId.Location = new System.Drawing.Point(589, 228);
+            this.txtSozlesmeId.Name = "txtSozlesmeId";
+            this.txtSozlesmeId.Size = new System.Drawing.Size(89, 20);
+            this.txtSozlesmeId.TabIndex = 110;
+            // 
+            // label12
+            // 
+            this.label12.Location = new System.Drawing.Point(497, 98);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(100, 23);
+            this.label12.TabIndex = 109;
+            this.label12.Text = "TCNO";
+            // 
+            // label11
+            // 
+            this.label11.Location = new System.Drawing.Point(497, 127);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(100, 23);
+            this.label11.TabIndex = 108;
+            this.label11.Text = "ADI";
+            // 
+            // label10
+            // 
+            this.label10.Location = new System.Drawing.Point(497, 156);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(100, 23);
+            this.label10.TabIndex = 107;
+            this.label10.Text = "DEĞER";
+            // 
+            // label9
+            // 
+            this.label9.Location = new System.Drawing.Point(497, 205);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(100, 23);
+            this.label9.TabIndex = 106;
+            this.label9.Text = "ÜLKE KODU";
+            // 
+            // label8
+            // 
+            this.label8.Location = new System.Drawing.Point(497, 179);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(100, 23);
+            this.label8.TabIndex = 105;
+            this.label8.Text = "YAŞ";
+            // 
+            // label7
+            // 
+            this.label7.Location = new System.Drawing.Point(497, 231);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(100, 23);
+            this.label7.TabIndex = 104;
+            this.label7.Text = "SÖZLEŞME ID";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Red;
+            this.button1.Location = new System.Drawing.Point(500, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(178, 69);
+            this.button1.TabIndex = 103;
+            this.button1.Text = "OYUNCU İŞLEMLERİ";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // btnAra
+            // 
+            this.btnAra.BackColor = System.Drawing.Color.OrangeRed;
+            this.btnAra.Location = new System.Drawing.Point(324, 413);
+            this.btnAra.Name = "btnAra";
+            this.btnAra.Size = new System.Drawing.Size(126, 23);
+            this.btnAra.TabIndex = 122;
+            this.btnAra.Text = "OYUNCU ARA";
+            this.btnAra.UseVisualStyleBackColor = false;
+            this.btnAra.Click += new System.EventHandler(this.btnAra_Click);
+            // 
+            // btnGuncelle
+            // 
+            this.btnGuncelle.BackColor = System.Drawing.Color.OrangeRed;
+            this.btnGuncelle.Location = new System.Drawing.Point(324, 384);
+            this.btnGuncelle.Name = "btnGuncelle";
+            this.btnGuncelle.Size = new System.Drawing.Size(126, 23);
+            this.btnGuncelle.TabIndex = 121;
+            this.btnGuncelle.Text = "OYUNCU GÜNCELLE";
+            this.btnGuncelle.UseVisualStyleBackColor = false;
+            this.btnGuncelle.Click += new System.EventHandler(this.btnGuncelle_Click);
+            // 
+            // btnSil
+            // 
+            this.btnSil.BackColor = System.Drawing.Color.OrangeRed;
+            this.btnSil.Location = new System.Drawing.Point(324, 355);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(126, 23);
+            this.btnSil.TabIndex = 120;
+            this.btnSil.Text = "OYUNCU SİL";
+            this.btnSil.UseVisualStyleBackColor = false;
+            this.btnSil.Click += new System.EventHandler(this.btnSil_Click);
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.BackColor = System.Drawing.Color.OrangeRed;
+            this.btnEkle.Location = new System.Drawing.Point(324, 326);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(126, 23);
+            this.btnEkle.TabIndex = 119;
+            this.btnEkle.Text = "OYUNCU EKLE";
+            this.btnEkle.UseVisualStyleBackColor = false;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // btnListele
+            // 
+            this.btnListele.BackColor = System.Drawing.Color.OrangeRed;
+            this.btnListele.Location = new System.Drawing.Point(324, 297);
+            this.btnListele.Name = "btnListele";
+            this.btnListele.Size = new System.Drawing.Size(126, 23);
+            this.btnListele.TabIndex = 118;
+            this.btnListele.Text = "OYUNCU LİSTELE";
+            this.btnListele.UseVisualStyleBackColor = false;
+            this.btnListele.Click += new System.EventHandler(this.btnListele_Click);
+            // 
+            // dgwOyuncu
+            // 
+            this.dgwOyuncu.AllowUserToAddRows = false;
+            this.dgwOyuncu.AllowUserToDeleteRows = false;
+            this.dgwOyuncu.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgwOyuncu.Location = new System.Drawing.Point(45, 12);
+            this.dgwOyuncu.Name = "dgwOyuncu";
+            this.dgwOyuncu.ReadOnly = true;
+            this.dgwOyuncu.Size = new System.Drawing.Size(405, 259);
+            this.dgwOyuncu.TabIndex = 124;
+            // 
+            // txtAra
+            // 
+            this.txtAra.Location = new System.Drawing.Point(229, 413);
+            this.txtAra.Name = "txtAra";
+            this.txtAra.Size = new System.Drawing.Size(89, 20);
+            this.txtAra.TabIndex = 125;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtAra);
+            this.Controls.Add(this.dgwOyuncu);
+            this.Controls.Add(this.btnAra);
+            this.Controls.Add(this.btnGuncelle);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.btnListele);
+            this.Controls.Add(this.txtTakimNo);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtTcno);
+            this.Controls.Add(this.txtAdi);
+            this.Controls.Add(this.txtDeger);
+            this.Controls.Add(this.txtYas);
+            this.Controls.Add(this.txtUlkeKodu);
+            this.Controls.Add(this.txtSozlesmeId);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgwOyuncu)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtTakimNo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtTcno;
+        private System.Windows.Forms.TextBox txtAdi;
+        private System.Windows.Forms.TextBox txtDeger;
+        private System.Windows.Forms.TextBox txtYas;
+        private System.Windows.Forms.TextBox txtUlkeKodu;
+        private System.Windows.Forms.TextBox txtSozlesmeId;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnAra;
+        private System.Windows.Forms.Button btnGuncelle;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.Button btnListele;
+        private System.Windows.Forms.DataGridView dgwOyuncu;
+        private System.Windows.Forms.TextBox txtAra;
+    }
+}
+
---
+++ futbolTakim/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using Npgsql;
+
+namespace futbolTakim
+{
+    public partial class Form1 : Form
+    {
+        NpgsqlConnection baglanti;
+        DataSet ds;
+        NpgsqlDataAdapter veriadaptoru;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        void oyuncuGetir()
+        {
+            string sorgu = "select *from oyuncu";
+            baglanti = new NpgsqlConnection("server=localHost;port=5432;Database=futboltakimlari; User ID=postgres; Password=mert1903");
+            veriadaptoru = new NpgsqlDataAdapter(sorgu, baglanti);
+            ds = new DataSet();
+            veriadaptoru.Fill(ds);
+            dgwOyuncu.DataSource = ds.Tables[0];
+        }
+
+        private void btnListele_Click(object sender, EventArgs e)
+        {
+            oyuncuGetir();
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            NpgsqlCommand komut1 = new NpgsqlCommand("insert into oyuncu(tcno,adi,deger,yasi,ulkekodu,sozlesmeid,takimno) values (@p1,@p2,@p3,@p4,@p5,@p6,@p7)",baglanti);
+            komut1.Parameters.AddWithValue("@p1", txtTcno.Text);
+            komut1.Parameters.AddWithValue("@p2", txtAdi.Text);
+            komut1.Parameters.AddWithValue("@p3", int.Parse(txtDeger.Text));
+            komut1.Parameters.AddWithValue("@p4", int.Parse(txtYas.Text));
+            komut1.Parameters.AddWithValue("@p5", int.Parse(txtUlkeKodu.Text));
+            komut1.Parameters.AddWithValue("@p6", int.Parse(txtSozlesmeId.Text));
+            komut1.Parameters.AddWithValue("@p7", int.Parse(txtTakimNo.Text));
+            komut1.ExecuteNonQuery();
+            baglanti.Close();
+        }
+
+        private void btnSil_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            NpgsqlCommand komut2 = new NpgsqlCommand("delete from oyuncu where tcno=@p1", baglanti);
+            komut2.Parameters.AddWithValue("@p1", txtTcno.Text);
+            komut2.ExecuteNonQuery();
+            baglanti.Close();
+        }
+
+        private void btnGuncelle_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            NpgsqlCommand komut3 = new NpgsqlCommand("update oyuncu set adi=@p1,deger=@p2,yasi=@p3,ulkekodu=@p4,sozlesmeid=@p5,takimno=@p6 where tcno=@p7", baglanti);
+            komut3.Parameters.AddWithValue("@p1", txtAdi.Text);
+            komut3.Parameters.AddWithValue("@p2", int.Parse(txtDeger.Text));
+            komut3.Parameters.AddWithValue("@p3", int.Parse(txtYas.Text));
+            komut3.Parameters.AddWithValue("@p4", int.Parse(txtUlkeKodu.Text));
+            komut3.Parameters.AddWithValue("@p5", int.Parse(txtSozlesmeId.Text));
+            komut3.Parameters.AddWithValue("@p6", int.Parse(txtTakimNo.Text));
+            komut3.Parameters.AddWithValue("@p7", txtTcno.Text);
+            komut3.ExecuteNonQuery();
+            baglanti.Close();
+        }
+
+        private void btnAra_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            NpgsqlCommand komut4 = new NpgsqlCommand("select *from oyuncu where adi like '%"+txtAra.Text+"%'", baglanti);
+            veriadaptoru = new NpgsqlDataAdapter(komut4);
+            ds = new DataSet();
+            veriadaptoru.Fill(ds);
+            dgwOyuncu.DataSource = ds.Tables[0];
+            baglanti.Close();
+
+        }
+    }
+    
+}
